4da8e06f249ebb5d231059f7ad58ca5ea03973a8
sovpro <its.so@outlook.com>
2020-01-27 09:52:49 -0500

    Optimize performance
    Reduce steps when not preserving integer value
    
    Replace remainder operator with manual equivalent
    
    Pre-compute powers of 10

--- unfloat.js
+++ unfloat.js
@@ -1,7 +1,10 @@
 "use strict"
-const getDigit = require ('@sovpro/get-digit')
 
 module.exports = unfloat
+
+const pow10s = new Array (
+  Math.ceil (Math.log10 (Number.MAX_VALUE))
+).fill (0).map ((_, i) => Math.pow (10, i))
 
 function unfloat (val, preserve) {
   if (val === null) return
@@ -11,18 +14,17 @@ function unfloat (val, preserve) {
   let num = 0
   let i = 0
   let intval_len = 0
+  let remainder = 0
   do {
-    num = val * Math.pow (10, ++i)
-  } while (num % 10 > 0)
-  if (intval >= 1 && (!!preserve === false)) {
-    intval_len = Math.ceil (Math.log10 (num))
-    while (Math.ceil (Math.log10 (num)) > i) {
-      num -= Math.pow (10, intval_len)
-           * getDigit (1 + intval_len--, num)
-    }
-  }
-  while (getDigit (1, num) === 0) {
-    num /= 10
+    num = val * pow10s[++i]
+    remainder = num / 10
+  } while ((remainder - Math.trunc (remainder)) !== 0)
+  if (intval >= 1 && (!!preserve === false))
+    num -= intval * pow10s[i]
+  remainder = num / 10
+  while ((remainder - Math.trunc (remainder)) === 0) {
+    num = remainder
+    remainder = num / 10
   }
   return num
 }
